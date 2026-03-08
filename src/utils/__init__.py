72c078541fe7b8ec554c49b3ce1333392a3be66c
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-03-09 00:46:01 +0600

    [fix]: lazy load PyQt6 imports for `__init__.py`;

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,12 +4,10 @@ Utilities package - helper functions and configuration
 
 from .config import AnalysisConfig, AppConfig, PathConfig, UIConfig, app_config
 from .resources import (count_files_with_extension, ensure_directory_exists,
-                        get_phylo_tree_directory, load_pixmap_safely,
-                        resource_path)
+                        get_phylo_tree_directory, resource_path)
 
 __all__ = [
     "resource_path",
-    "load_pixmap_safely",
     "ensure_directory_exists",
     "count_files_with_extension",
     "get_phylo_tree_directory",
