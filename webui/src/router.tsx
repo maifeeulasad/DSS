49beecc8f648ce1fdd3570255645012138e83c38
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-03-11 20:09:54 +0600

    [refactor]: fix formatting and add semicolons in router.tsx;

--- webui/src/router.tsx
+++ webui/src/router.tsx
@@ -1,13 +1,13 @@
-import { createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import { createRouter } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
 
 export function getRouter() {
   const router = createRouter({
     routeTree,
     scrollRestoration: true,
-  })
+  });
 
-  return router
+  return router;
 }
 
 declare module '@tanstack/react-router' {
