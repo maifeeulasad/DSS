f5ba3876a10ac97d916c4f1b09c7e4610a0afe57
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-03-11 19:59:19 +0600

    [refactor]: seo support;

--- webui/src/routes/__root.tsx
+++ webui/src/routes/__root.tsx
@@ -1,15 +1,35 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+/// <reference types="vite/client" />
+import { createRootRoute, HeadContent, Outlet, Scripts } from '@tanstack/react-router'
 import React from 'react'
 import '../index.css'
 
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      { charSet: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'theme-color', content: '#000000' },
+      { name: 'description', content: 'DSS - DNA Sequence Similarities' },
+    ],
+    links: [
+      { rel: 'icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', href: '/logo192.png' },
+      { rel: 'manifest', href: '/manifest.json' },
+    ],
+  }),
   component: RootLayout,
 })
 
 export default function RootLayout() {
   return (
-    <>
-      <Outlet />
-    </>
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <Outlet />
+        <Scripts />
+      </body>
+    </html>
   )
 }
