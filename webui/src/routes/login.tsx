3230d71c7855be0a88887d29b37af5bd73b40ed0
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-03-11 14:14:28 +0600

    [ui]: test login page;

---
+++ webui/src/routes/login.tsx
@@ -0,0 +1,11 @@
+// Static services page route
+// Route: /login (handled in src/index.tsx)
+
+export default function Login() {
+
+  return (
+    <div className="space-y-8 py-12">
+      login page
+    </div>
+  )
+}
